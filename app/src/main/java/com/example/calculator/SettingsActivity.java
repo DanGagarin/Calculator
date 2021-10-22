7c2571cb013ba1cf0985006210ab1c636f9f5970
DanGagarin <gagarindan@yandex.ru>
2021-10-22 15:00:46 +0300

    activity_calculator_02

--- app/src/main/java/com/example/calculator/SettingsActivity.java
+++ app/src/main/java/com/example/calculator/SettingsActivity.java
@@ -1,30 +1,19 @@
 package com.example.calculator;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.btn_settings).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
-
-            }
-        });
 
         if (savedInstanceState == null) {
             logLifecycle("onCreate for the first time");
@@ -84,11 +73,5 @@ public class MainActivity extends AppCompatActivity {
 
     private void logLifecycle(String method) {
         Log.d("LifecycleMain", method + " " + this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        Toast.makeText(this, "Back pressed", Toast.LENGTH_SHORT).show();
-        finish();
     }
 }
